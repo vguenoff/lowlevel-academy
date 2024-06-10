65f6ae574895d2461517348d04dfb51911167aed
vguenoff <vguenoff@gmail.com>
2024-06-10 19:15:39 +0300

    feat: include math header

--- zero-hero-c-programming/src/main.c
+++ zero-hero-c-programming/src/main.c
@@ -2,9 +2,14 @@
 // invoking the preprocessor
 // copying the header file in out code
 // giving us functions like printf
+#include "math.h"
 
 int main() {
   printf("Hello, World!\n");
+  int a = 15;
+  int b = 18;
+
+  printf("a is %d, b is %d -> a + b is %d\n", a, b, add(a, b));
 
   return 0;
 }
