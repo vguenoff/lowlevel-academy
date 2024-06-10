600c4e2bddb2269fb03d338afb976e8f98c5c21f
vguenoff <vguenoff@gmail.com>
2024-06-10 19:04:38 +0300

    feat: initial commit and setup

---
+++ zero-hero-c-programming/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// invoking the preprocessor
+// copying the header file in out code
+// giving us functions like printf
+
+int main() {
+  printf("Hello, World!\n");
+
+  return 0;
+}
