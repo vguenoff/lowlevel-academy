de47f685f638b021e5d0aed825be276a96dd5b1a
vguenoff <vguenoff@gmail.com>
2024-06-12 08:19:55 +0300

    feat: add da2 2 challenge

--- zero-hero-c-programming/src/05-strings.c
+++ zero-hero-c-programming/src/05-strings.c
@@ -26,5 +26,12 @@ int strings05() {
   // strcpy(dest, src);
   // strncpy(dest, src, n);
 
+  char name[50];
+
+  printf("Enter your name: ");
+  scanf("%s", name);
+
+  printf("Hello %s!\n", name);
+
   return 0;
 }
