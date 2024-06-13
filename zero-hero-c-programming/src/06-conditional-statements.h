73c5b4cff1e4187cacc391bd723e33044e8112ba
vguenoff <vguenoff@gmail.com>
2024-06-13 16:43:55 +0300

    feat: conditionals

---
+++ zero-hero-c-programming/src/06-conditional-statements.h
@@ -0,0 +1,1 @@
+int conditionalStatements06();
--- zero-hero-c-programming/src/main.c
+++ zero-hero-c-programming/src/main.c
@@ -1,6 +1,7 @@
 // include local file header
 // #include "04-array.h"
-#include "05-strings.h"
+// #include "05-strings.h"
+#include "06-conditional-statements.h"
 
 // #include "math.h"
 // invoking the preprocessor
@@ -10,7 +11,8 @@
 
 int main() {
   // array04();
-  strings05();
+  // strings05();
+  conditionalStatements06();
 
   return 0;
 }
